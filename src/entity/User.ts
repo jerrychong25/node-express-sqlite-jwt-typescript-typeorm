4c08f1d45c37e5b83d0f0fc52bcdaba263ca5ce2
jerrychong25 <15714095+jerrychong25@users.noreply.github.com>
2019-05-30 12:49:13 +0800

    Fixed few bugs
    -Added jwtSecret in config/config.ts
    -Removed controller/UserController.ts
    -Added AuthController class in controllers/AuthController.ts
    -Added UserController class in controllers/UserController.ts
    -Modified User class in entity/User.ts
    -Added Middleware checkJwt.ts and checkRole.ts
    -Added migration CreateAdminUser.ts

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,45 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
-
-@Entity()
-export class User {
-
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    Unique,
+    CreateDateColumn,
+    UpdateDateColumn
+  } from "typeorm";
+  import { Length, IsNotEmpty } from "class-validator";
+  import * as bcrypt from "bcryptjs";
+  
+  @Entity()
+  @Unique(["username"])
+  export class User {
     @PrimaryGeneratedColumn()
     id: number;
-
+  
     @Column()
-    firstName: string;
-
+    @Length(4, 20)
+    username: string;
+  
     @Column()
-    lastName: string;
-
+    @Length(4, 100)
+    password: string;
+  
     @Column()
-    age: number;
-
-}
+    @IsNotEmpty()
+    role: string;
+  
+    @Column()
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @Column()
+    @UpdateDateColumn()
+    updatedAt: Date;
+  
+    hashPassword() {
+      this.password = bcrypt.hashSync(this.password, 8);
+    }
+  
+    checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+      return bcrypt.compareSync(unencryptedPassword, this.password);
+    }
+  }
