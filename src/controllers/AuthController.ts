cd8f98d7b136b4b6c6cb560640a4857345f83421
Jerry Chong <jerrychong25@gmail.com>
2020-05-11 17:02:24 +0800

    Added error logs and response error messages

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -8,49 +8,58 @@ import config from "../config/config";
 
 class AuthController {
   static login = async (req: Request, res: Response) => {
-    //Check if username and password are set
-    let { name, password } = req.body;
+    const { name, password } = req.body;
+
+    // Check if username and password are set
     if (!(name && password)) {
-      res.status(400).send();
+      console.error('Empty name or password!');
+      return res.status(400).send({
+        error: 'Empty name or password'
+      });
     }
 
-    //Get user from database
+    // Get user from database
     const userRepository = getRepository(User);
     let user: User;
     try {
       user = await userRepository.findOneOrFail({ where: { name } });
     } catch (error) {
-      res.status(401).send();
+      console.error('Cant find user from DB!');
+      return res.status(401).send({
+        error: 'Cant find user from DB'
+      });
     }
 
-    //Check if encrypted password match
+    // Check if encrypted password match
     if (!user.checkIfUnencryptedPasswordIsValid(password)) {
-      res.status(401).send();
-      return;
+      console.error('Cannot match encrypted password!');
+      return res.status(401).send({
+        error: 'Cannot match encrypted password'
+      });
     }
 
-    //Sing JWT, valid for 1 hour
+    // Sing JWT, valid for 1 hour
     const token = jwt.sign(
       { userId: user.id, username: user.name },
       config.jwtSecret,
       { expiresIn: "1h" }
     );
 
-    //Send the jwt in the response
+    // Send the jwt in the response
     res.send(token);
   };
 
   static changePassword = async (req: Request, res: Response) => {
-    //Get ID from JWT
+    // Get ID from JWT
     const id = res.locals.jwtPayload.userId;
 
-    //Get parameters from the body
+    // Get parameters from the body
     const { oldPassword, newPassword } = req.body;
     if (!(oldPassword && newPassword)) {
       res.status(400).send();
     }
 
-    //Get user from the database
+    // Get user from the database
     const userRepository = getRepository(User);
     let user: User;
     try {
@@ -59,24 +68,25 @@ class AuthController {
       res.status(401).send();
     }
 
-    //Check if old password matchs
+    // Check if old password matchs
     if (!user.checkIfUnencryptedPasswordIsValid(oldPassword)) {
       res.status(401).send();
       return;
     }
 
-    //Validate de model (password lenght)
+    // Validate de model (password lenght)
     user.password = newPassword;
     const errors = await validate(user);
     if (errors.length > 0) {
       res.status(400).send(errors);
       return;
     }
-    //Hash the new password and save
+    // Hash the new password and save
     user.hashPassword();
     userRepository.save(user);
 
     res.status(204).send();
   };
 }
+
 export default AuthController;
